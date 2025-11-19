2ca2082725b41f051d85d022541b6f251a4dbd45
Rifqi Fitri <rifqifitriansyah28@gmail.com>
2025-11-19 15:32:27 +0800

    Update while6 sumbulat.php

--- 07_while/while6 sumbulat.php
+++ 07_while/while6 sumbulat.php
@@ -5,5 +5,6 @@ while ($bil <= 127) {
     if ($bil % 6 == 0) $hitung = $hitung + 1;
     $bil++;
 }
-echo "Banyaknya bilangan bulat adalah ".$hitung:
+echo "Banyaknya bilangan bulat adalah ".$hitung;
+
 ?>
